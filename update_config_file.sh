7b836ad501f0321cad9995e2c2900df7410c6c3e
mianbo.huang.gigabyte <mianbo.huang@hotmail.com>
2019-10-19 12:21:25 +0800

    update by adding gigabayte's cygwin64 bash profile

--- update_config_file.sh
+++ update_config_file.sh
@@ -16,10 +16,16 @@ if [ $? != 0 ];then
     cp $dir/d/Vim/_vimrc ./_vimrc-$HOSTNAME
 fi
 
-
 diff $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
 
 if [  $? != 0 ];then
     echo copy tmux .tmux.conf ...
     cp $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
+fi
+
+diff $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
+
+if [  $? != 0 ];then
+    echo copy cygwin64 .bash_profile ...
+    cp  $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
 fi
