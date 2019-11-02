731a78ea521d7efb9dd57a3c06c18e0e8de3e8bd
deepblue0755 <mianbo.huang@hotmail.com>
2019-11-02 10:32:12 +0800

    first add config file from MacOSx

--- update_config_file.sh
+++ update_config_file.sh
@@ -1,48 +1,89 @@
 #!/bin/bash
 
+function copy_config_from_macosx()
+{
+    diff ~/.bash_profile ./_bash_profile-$HOSTNAME
+    flag=0
 
-if [ "$shell" == "/usr/bin/bash" ];then
-    dir=/cygdrive
+    if [  $? != 0 ];then
+        echo copy MacOSx .bash_profile ...
+        cp  ~/.bash_profile ./_bash_profile-$HOSTNAME
+        flag=1
+    fi
 
-else
-    dir=
-fi
+    if [ $flag = 0 ];then
+        echo nothing to update from MacOSx
+    fi
 
+    return 0
 
-diff $dir/d/Vim/_vimrc ./_vimrc-$HOSTNAME
-
-if [ $? != 0 ];then
-    echo copy vim _vimrc ...
-    cp $dir/d/Vim/_vimrc ./_vimrc-$HOSTNAME
-fi
-
-diff $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
-
-if [  $? != 0 ];then
-    echo copy tmux .tmux.conf ...
-    cp $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
-fi
-
-diff $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
-
-if [  $? != 0 ];then
-    echo copy cygwin64 .bash_profile ...
-    cp  $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
-fi
+}
 
 
+function copy_config_from_cygwin()
+{
+    if [ "$shell" == "/usr/bin/bash" ];then
+        dir=/cygdrive
 
-diff $dir/d/cygwin64/home/mianb/.gitconfig ./_gitconfig-$HOSTNAME
-
-if [  $? != 0 ];then
-    echo copy cygwin64 .gitconfig ...
-    cp  $dir/d/cygwin64/home/mianb/.gitconfig ./_gitconfig-$HOSTNAME
-fi
+    else
+        dir=
+    fi
 
 
-diff $dir/d/Batch/_cvimrc ./_cvimrc-$HOSTNAME
+    diff $dir/d/Vim/_vimrc ./_vimrc-$HOSTNAME
 
-if [  $? != 0 ];then
-    echo copy _cvimrc ...
-    cp  $dir/d/Batch/_cvimrc ./_cvimrc-$HOSTNAME
-fi
+    if [ $? != 0 ];then
+        echo copy vim _vimrc ...
+        cp $dir/d/Vim/_vimrc ./_vimrc-$HOSTNAME
+    fi
+
+    diff $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
+
+    if [  $? != 0 ];then
+        echo copy tmux .tmux.conf ...
+        cp $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
+    fi
+
+    diff $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
+
+    if [  $? != 0 ];then
+        echo copy cygwin64 .bash_profile ...
+        cp  $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
+    fi
+
+    diff $dir/d/cygwin64/home/mianb/.gitconfig ./_gitconfig-$HOSTNAME
+
+    if [  $? != 0 ];then
+        echo copy cygwin64 .gitconfig ...
+        cp  $dir/d/cygwin64/home/mianb/.gitconfig ./_gitconfig-$HOSTNAME
+    fi
+
+    diff $dir/d/Batch/_cvimrc ./_cvimrc-$HOSTNAME
+
+    if [  $? != 0 ];then
+        echo copy _cvimrc ...
+        cp  $dir/d/Batch/_cvimrc ./_cvimrc-$HOSTNAME
+    fi
+}
+
+function main()
+{
+    echo Running Scripts At $HOSTNAME !!
+    case $HOSTNAME in 
+
+        Gigabyte-Brix)
+            copy_config_from_cygwin
+        ;;
+
+        Huangs-T580)
+            copy_config_from_cygwin
+        ;;
+
+        Huangs-MBP)
+            copy_config_from_macosx
+        ;;
+    esac
+}
+
+
+main
