0f87aa9e644f3b09a854989e44229f9b0423d3b5
mianbo.huang <mianbo.huang@hotmail.com>
2019-11-03 10:17:18 +0000

    udpate config file from mianb @ Huangs-T580

--- update_config_file.sh
+++ update_config_file.sh
@@ -24,6 +24,8 @@ function copy_config_from_macosx()
 
 function copy_config_from_cygwin()
 {
+    flag=0
+
     if [ "$shell" == "/usr/bin/bash" ];then
         dir=/cygdrive
 
@@ -37,6 +39,7 @@ function copy_config_from_cygwin()
     if [ $? != 0 ];then
         echo copy vim _vimrc ...
         cp $dir/d/Vim/_vimrc ./_vimrc-$HOSTNAME
+        flag=1
     fi
 
     diff $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
@@ -44,6 +47,7 @@ function copy_config_from_cygwin()
     if [  $? != 0 ];then
         echo copy tmux .tmux.conf ...
         cp $dir/d/cygwin64/home/$USER/.tmux.conf ./_tmux.conf-$HOSTNAME
+        flag=1
     fi
 
     diff $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
@@ -51,6 +55,7 @@ function copy_config_from_cygwin()
     if [  $? != 0 ];then
         echo copy cygwin64 .bash_profile ...
         cp  $dir/d/cygwin64/home/mianb/.bash_profile ./_bash_profile-$HOSTNAME
+        flag=1
     fi
 
     diff $dir/d/cygwin64/home/mianb/.gitconfig ./_gitconfig-$HOSTNAME
@@ -58,6 +63,7 @@ function copy_config_from_cygwin()
     if [  $? != 0 ];then
         echo copy cygwin64 .gitconfig ...
         cp  $dir/d/cygwin64/home/mianb/.gitconfig ./_gitconfig-$HOSTNAME
+        flag=1
     fi
 
     diff $dir/d/Batch/_cvimrc ./_cvimrc-$HOSTNAME
@@ -65,6 +71,11 @@ function copy_config_from_cygwin()
     if [  $? != 0 ];then
         echo copy _cvimrc ...
         cp  $dir/d/Batch/_cvimrc ./_cvimrc-$HOSTNAME
+        flag=1
+    fi
+
+    if [ "$flag" == "0" ];then
+        echo no update from $HOSTNAME
     fi
 }
 
