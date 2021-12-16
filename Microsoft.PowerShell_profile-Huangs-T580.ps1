030b881fc5183cdf84cea7f727618b8f1d0c525b
mianbo.huang.t580 <mianbo.huang@hotmail.com>
2021-12-16 01:43:12 +0000

    update file  Microsoft.PowerShell_profile-Huangs-T580.ps1 _bash_profile-Huangs-T580 _ssh_config-Huangs-T580 _vimrc-Huangs-T580 from mianb@Huangs-T580

--- Microsoft.PowerShell_profile-Huangs-T580.ps1
+++ Microsoft.PowerShell_profile-Huangs-T580.ps1
@@ -1,5 +1,13 @@
 $Env:Path += ";D:\vim\vim82"
 $Env:Path += ";D:\cygwin64\bin"
 Set-Alias which Get-Command
+Set-Alias ifconfig ipconfig
+Set-Alias alias Set-Alias
+Set-PSReadlineOption -EditMode Vi
 function touch {set-content -Path ($args[0]) -Value ($null)} 
-function reload { &$profile }
+function reload { .$profile }
+function config { vim $profile }
+function nuc11 { Enter-PSSession -ComputerName NUC11 }
+function cygwin { D:\cygwin64\bin\mintty -i /Cygwin-Terminal.ico -  }
+function windowsterminal { D:\batch\start_win_store_app.ps1 -AppName WindowsTerminal }
+
