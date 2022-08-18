616a481db405850ed76ba5dc73506ee7456dd42e
mianbo.huang.t580 <mianbo.huang@hotmail.com>
2022-08-18 07:57:01 +0100

    NEW: new batch scripts update_config_file.bat

---
+++ update_config_file.bat
@@ -0,0 +1,42 @@
+@echo off
+set GIT=D:\Git\bin\git.exe
+
+if not exist "%GIT%" (
+    echo ERROR: could  not find the git command
+    goto end
+)
+
+echo INFO: git pull to latest git version ...
+pushd D:\documents\11-configs-from-github
+%GIT% pull origin master
+popd
+
+call :update_config_file
+goto end
+
+rem -----------------------------------------------------------
+rem Function update_config_file
+rem -----------------------------------------------------------
+:update_config_file
+echo INFO: Compare files to see if update is needed
+fc D:\cygwin64\home\mianb\.bash_profile D:\documents\11-configs-from-github\_bash_profile-Huangs-T580 > NUL
+if %errorlevel% EQU 0 (
+    echo INFO: No update has been found for cygwin64 bash_profile
+) else (
+    xcopy /y D:\cygwin64\home\mianb\.bash_profile D:\documents\11-configs-from-github\_bash_profile-Huangs-T580
+    echo INFO: "git update to origin ..."
+    pushd D:\documents\11-configs-from-github
+    %GIT% add _bash_profile-Huangs-T580
+    %GIT% commit -m "UPDATE cygwin64 bash_profile from machine %COMPUTERNAME%"
+    for /f %%i in ( '%GIT% remote' ) do (
+        echo "INFO: git push to %%i" 
+        %GIT% push -u %%i  master
+    )
+    popd
+)
+goto:EOF
+
+
+:end
+echo INFO: Job is done, bye bye
+exit /b 0
