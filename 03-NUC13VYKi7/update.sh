8fa089bfb949e6b97d4190ec79d4c0aa96d9ecd0
mianbo.huang.nuc13vyki7 <mianbo.huang@hotmail.com>
2024-08-05 16:52:45 +0800

    UPDATE: Refactor _bashrc,  update.sh,  vim_plugin_git_repo.txt, from NUC13VYKi7

--- 03-NUC13VYKi7/update.sh
+++ 03-NUC13VYKi7/update.sh
@@ -26,6 +26,19 @@ function PWARN() {
     return 0
 }
 
+function list_vim_plugins()
+{
+    pushd /home/$(id -nu 1000)/.vim/bundle/ &>/dev/null || exit 1
+
+    for fd in $(find ./ -maxdepth 1 -type d | grep -v "\./$")
+    do
+        pushd $fd &>/dev/null || exit 1
+            git remote -v | grep origin | sed -n 1p | sort | awk '{ print $2 }'
+        popd &>/dev/null || exit 1
+    done
+    popd &>/dev/null || exit 1
+}
+
 
 workdir="/home/$(id -nu 1000)/documents/11-configs-from-github/03-NUC13VYKi7/"
 
@@ -34,20 +47,41 @@ if [[ ! -d "${workdir}" ]];then
     exit 1
 fi
 
-PINFO "dpkg -l > ${workdir}/dpkg-lists.txt"
-dpkg -l > "${workdir}/dpkg-lists.txt"
+PINFO "git pull ..."
+if ! git pull &>/dev/null;then
+    PERROR "git pull failed"
+else
+    PINFO "successfully git pull"
+fi
 
-PINFO "apt list --installed > ${workdir}/deb-packages-list.txt ..."
-apt list --installed > "${workdir}/deb-packages-list.txt"
+
+PINFO "check if _bashrc need to updated"
+if diff -q ${workdir}/_bashrc /home/$(id -nu 1000)/.bashrc;then
+    PINFO "no need to update _bashrc ..."
+else
+    PINFO "update _bashrc ..."
+    cp -fv /home/$(id -nu 1000)/.bashrc _bashrc
+fi
 
 
+if ! diff -q <(dpkg -l) ${workdir}/dpkg-lists.txt;then
+    PINFO "dpkg -l > ${workdir}/dpkg-lists.txt"
+    dpkg -l > "${workdir}/dpkg-lists.txt"
+else
+    PINFO "no need to update dpkg list"
+fi
+
+if ! diff -q <(apt list --installed) ${workdir}/deb-packages-list.txt;then
+    PINFO "apt list --installed > ${workdir}/deb-packages-list.txt ..."
+    apt list --installed > "${workdir}/deb-packages-list.txt"
+else
+    PINFO "no need to update apt installed list"
+fi
+
+if ! diff -q <(list_vim_plugins | sort) "${workdir}/vim_plugin_git_repo.txt";then
 PINFO "update vim plugin list ..."
-truncate -s 0 "${workdir}/vim_plugin_git_repo.txt"
-pushd ~/.vim/bundle/ &>/dev/null || exit 1
-for folder in  $(find ./ -maxdepth 1 -type d | grep -v "\./$")
-do
-    pushd $folder &>/dev/null || exit 1
-        git remote -v | grep origin | sed -n 1p | awk '{ print $2 }' | tee -a ${workdir}/vim_plugin_git_repo.txt
-    popd &>/dev/null || exit 1
-done
-popd &>/dev/null || exit 1
+    truncate -s 0 "${workdir}/vim_plugin_git_repo.txt"
+    list_vim_plugins  | sort | tee -a "${workdir}/vim_plugin_git_repo.txt" 
+else
+    PINFO "no need to update vim plugin list"
+fi
